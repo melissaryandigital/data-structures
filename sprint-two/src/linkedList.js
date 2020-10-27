6a5ffed908ff4c12a2755e36d81e5c36240a6048
Melissa Ryan <melissaryandigital@gmail.com>
2020-10-26 23:08:11 -0500

    Complete Linked List

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,42 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    var newNode = new Node(value);
+
+    if (list.head === null && list.tail === null ) {
+      list.head = newNode;
+      list.tail = list.head;
+    } else {
+      var currentNode = list.head;
+      while (currentNode.next !== null) {
+        currentNode = currentNode.next;
+      }
+      currentNode.next = newNode;
+      list.tail = currentNode.next;
+    }
   };
 
   list.removeHead = function() {
+    console.log(list.head.next);
+    if (list.head === null) {
+      return;
+    } else {
+      var copy = list.head;
+      list.head = list.head.next;
+      return copy.value;
+    }
   };
 
   list.contains = function(target) {
+
+    var currentNode = list.head;
+    while (currentNode !== null) {
+      if ( currentNode.value === target) {
+        return true;
+      }
+      currentNode = currentNode.next;
+    }
+    return false;
   };
 
   return list;
@@ -23,7 +53,6 @@ var Node = function(value) {
 
   return node;
 };
-
 /*
  * Complexity: What is the time complexity of the above functions?
  */
--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,19 +1,36 @@
-var Tree = function(value) {
+var Tree = function (value) {
   var newTree = {};
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];
+  _.extend(newTree, treeMethods);
 
   return newTree;
 };
 
 var treeMethods = {};
 
-treeMethods.addChild = function(value) {
+treeMethods.addChild = function (value) {
+
+  var newTree = new Tree(value);
+  this.children.push(newTree);
+
 };
 
-treeMethods.contains = function(target) {
+treeMethods.contains = function (target) {
+
+  for (var i = 0; i < this.children.length; i++) {
+
+    if (this.children[i].value === target) {
+      return true;
+    }
+    if (this.children[i].contains(target) === true) {
+      return true;
+    }
+  }
+
+  return false;
 };
 
 
